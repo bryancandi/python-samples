4f95985a0e9b3dc2c66e42b4cbebf77dbfc85181
Bryan C <quakeaz@gmail.com>
2023-01-24 11:28:49 -0700

    Update separators

--- hello_color.py
+++ hello_color.py
@@ -1,8 +1,8 @@
 name = input("\nWhat is your name?\n")
-print("Hello, " + (name) + "!\n")
+print("Hello,", (name) + "!\n")
 
 color = input("What is your favorite color?\n")
-print((name) + "'s favorite color is " + (color) + ".\n")
+print((name) + "'s favorite color is", (color) + ".\n")
 
 mycolor = "red"
 mycolorguess = input("Can you guess my favorite color?\n").lower()
@@ -12,6 +12,6 @@ while mycolorguess != mycolor:
 	print("That is not correct!")
 	mycolorguess = input("Try again? Or type 'stop' to give up.\n").lower()
 else:
-	print("That is correct! " + (mycolor).title() + " is my favorite color!")
+	print("That is correct!", (mycolor).title(), "is my favorite color!")
 
 print("\nEnd.")
